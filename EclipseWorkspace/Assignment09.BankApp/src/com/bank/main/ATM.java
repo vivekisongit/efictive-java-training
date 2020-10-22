ace5414063a67337dc11ce148f952bd235de42ec
Vivek Kumar <Vivek.Kumar3@Ltts.com>
2020-10-22 14:32:54 +0530

    Assignment09 submitted

---
+++ EclipseWorkspace/Assignment09.BankApp/src/com/bank/main/ATM.java
@@ -0,0 +1,71 @@
+package com.bank.main;
+
+import com.bank.module.Bank;
+import com.bank.utils.Input;
+
+public class ATM {
+	
+	 
+	public void startExecution(){
+		 boolean quit=false;
+		 Input input=new Input();	
+		 Bank bank=new Bank();	 
+			
+		 while (!quit) {				
+				int choice = 0;
+				int accountNo=0;
+				int amt=0;
+				choice = input.readInt(
+						"0. Create Account\n1. Deposit\n2. Withdraw\n3. Show\n4. Change Password\n5. Credit interest\n6. Exit\nChoose:");
+
+				switch (choice) {	
+				case 0:
+					String  name = input.readString("Please enter name");
+					String  pass = input.readString("Please enter password");
+					amt=input.readInt("Please enter amt");
+					bank.openAccount(name, pass, amt);
+					
+					break;
+				case 1:		
+					accountNo=input.readInt("Please enter account Number");
+					amt = input.readInt("Please enter amount");
+					
+					bank.deposit(accountNo, amt);
+					break;
+				case 2:
+					accountNo = input.readInt("Please enter account number");
+					amt= input.readInt("Please enter amount");
+					
+					String passwordToWithdraw= input.readString("Please enter password");
+					bank.withdraw(accountNo, passwordToWithdraw, amt);
+					break;
+				case 3:
+					accountNo = input.readInt("Please enter account number");
+					bank.show(accountNo);
+					break;
+				case 4:
+					accountNo = input.readInt("Please enter account number");
+					String oldPassword= input.readString("Please enter old password");
+					String newPassword= input.readString("Please enter new password");
+					boolean b=bank.changePassword(accountNo,oldPassword, newPassword);
+					if(b) {
+						System.out.println("Password updated successfully");
+					}else {
+						System.out.println("Wrong old password");
+					}
+					break;
+				case 5:		
+					accountNo = input.readInt("Please enter account number");
+					bank.creditInterest(accountNo);					
+					break;
+				case 6:
+					quit = true;
+					System.out.println("\nThank You. Quitting the Application Now...");
+					break;
+				default:
+					break;
+				}
+			}
+	}
+
+}
---
+++ EclipseWorkspace/Assignment09.BankApp/src/com/bank/module/Bank.java
@@ -0,0 +1,84 @@
+package com.bank.module;
+
+public class Bank {
+	static int accountNum=1;
+	double interestRate=10;
+	
+	BankAccount bankAccountArr[]=new BankAccount[10];
+	
+	/*Interest should be change*/
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public  void openAccount(String name, String password, double amount) {		
+		BankAccount bankAccount=new BankAccount(name, password,amount,this.interestRate);
+		for(int i=0; i<10; i++) {
+			if(bankAccountArr[i]==null) {
+				bankAccountArr[i]=bankAccount;
+			}
+		}
+		System.out.println("Account opened successfully");
+		System.out.println("Your account no is:"+bankAccount.accountNumber);
+		
+	}
+	
+	public String withdraw(int accNo, String pass, double amt) {
+		String result="";
+		for(int i=0; i<10; i++) {
+			BankAccount bankAccount= (BankAccount)bankAccountArr[i];
+			if(bankAccount.getAccountNumber()==accNo){
+				result=bankAccount.withdraw((int)amt, pass);
+			}
+		}
+		return result;
+	}
+	public String deposit(int accNo, double amt) {
+		String result="";
+		for(int i=0; i<10; i++) {
+			BankAccount bankAccount= (BankAccount)bankAccountArr[i];
+			if(bankAccount.getAccountNumber()==accNo){
+				result=bankAccount.deposit((int)amt);
+			}
+		}
+		return result;
+	}
+	public boolean changePassword(int accNo, String oldPass, String newPass) {
+		boolean result=false;
+		for(int i=0; i<10; i++) {
+			BankAccount bankAccount= (BankAccount)bankAccountArr[i];
+			if(bankAccount.getAccountNumber()==accNo){
+				result=bankAccount.changePassword(oldPass, newPass);
+				if(result) {
+					System.out.println("Password changes successfully");
+				}
+			}
+		}
+		return result;
+	}
+	public void creditInterest(int accNo) {
+		for(int i=0; i<10; i++) {
+			BankAccount bankAccount= (BankAccount)bankAccountArr[i];
+			if(bankAccount.getAccountNumber()==accNo){
+				bankAccount.creditInterest();
+				
+			}
+		}
+	}
+	public String show(int accNo) {
+		String result="Error";
+		for(int i=0; i<10; i++) {
+			BankAccount bankAccount= (BankAccount)bankAccountArr[i];
+			if(bankAccount.getAccountNumber()==accNo){
+				result= bankAccount.showDetails();
+				
+			}
+		}
+		System.out.println(result);
+		return result;
+	}
+}
